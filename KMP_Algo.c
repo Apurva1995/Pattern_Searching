fbc6859b7f033208049cceffb0e26718968fcced
Apurva1995 <42551079+Apurva1995@users.noreply.github.com>
2018-09-23 22:56:20 +0530

    KMP algorithm
    Time Complexity : O(n) - for searching, where n is size of text
                                  O(m) - for creating lps array, where m is size of pat.

---
+++ KMP_Algo.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+
+void findIndexesOfPat(char *text, char *pat, int *lps) {
+    
+    int len1, len2, j, i;
+    
+    i = 0;
+    j = 1;
+    len1 = strlen(text);
+    len2 = strlen(pat);
+    
+    while(i<=(len1-len2)) {
+        
+        j = lps[j-1];
+        while(j < len2) {
+            
+            if(text[i] == pat[j]) {
+                i++;
+                j++;
+            }
+            else if(j > 0)
+                j = lps[j-1];
+            else
+                i++;
+        }
+        
+        if(j == len2)
+            printf("Pattern found at %d\n", (i-j));
+    }
+}
+
+void preprocess(int *lps, char *pat) {
+    
+    int len = strlen(pat);
+    lps[0] = 0;
+    
+    for(int i=1;i<len;i++) {
+        
+        if(pat[lps[i-1]] == pat[i])
+            lps[i] = lps[i-1] + 1;
+        else
+            lps[i] = 0;
+    }
+}
+
+int main(void) {
+	
+	char text[] = "ABABDABACDABABCABAB";
+	char pat[] = "ABABCABAB";
+	int lps[strlen(pat)];
+	
+	preprocess(lps, pat);
+	findIndexesOfPat(text, pat, lps);
+	
+	return 0;
+}
+
